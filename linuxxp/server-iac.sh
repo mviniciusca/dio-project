76567063612ff9bf2fe7f03d5670f431f537d82f
Marcos Coelho <mviniciusca@gmail.com>
2022-08-23 13:49:35 -0300

    new shell script to crate an web server in linux/wsl

---
+++ linuxxp/server-iac.sh
@@ -0,0 +1,14 @@
+echo "server updating ..."
+apt-get update
+apt-get upgrade -y
+echo "installing apache and unzip"
+apt-get install apache2 -y
+apt-get install unzip -y
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+echo "finished!"
+
+
